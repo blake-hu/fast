67680c47efd5a8e58b9c01d38f920323664f5f88
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2023-11-11 22:30:13 -0600

    Add jupyter notebook for CLS embeddings from transformers

--- fast/utils/cls.py
+++ fast/utils/cls.py
@@ -1,21 +1,16 @@
 # Define a function to extract CLS embeddings from input text
-def extract_cls_embeddings(model, text_array):
+def extract_cls_embeddings(model_output):
     '''
     Get CLS embeddings for each text item in the array.
     Args:
-    - text_array (list): List of input text.
+    - model_outputs (dictionary): Outputs of model, containing last_hidden_state, pooler_output, hidden_states, attentions.
     Returns:
     - NumPy array (len(text_array), 768): CLS embeddings (768,) for each text.
     '''
-    # Tokenize sentences
-    encoded_input = tokenizer(text_array, padding=True, truncation=True, return_tensors='pt')
-
-    # Compute token embeddings
-    with torch.no_grad():
-        model_output = model(**encoded_input) 
     # Model_output is a dictionary with 'last_hidden_state' key that stores final hidden states (all token embeddings)
-    last_hidden_state = model_output['last_hidden_state'] # Same as model_output[0]
-   
+    # Same as model_output[0]
+    last_hidden_state = model_output['last_hidden_state']
+
     # Extract the CLS embedding (index 0) from the last hidden state (3D tensor)
     cls_embedding = last_hidden_state[:, 0, :]
     # keep all data in batch, keep all hidden state features, but select first element from hidden states (hidden state corresponding to [CLS] token)
@@ -24,9 +19,3 @@ def extract_cls_embeddings(model, text_array):
     cls_embedding = cls_embedding.numpy()
 
     return cls_embedding
-
-# Example
-def cls_embedding_example(model):
-    examples = ["sentjnmnsd.", "!!kskfjka@"]
-    cls_embeddings_array = extract_cls_embeddings(model, examples)
-    print(cls_embeddings_array)
